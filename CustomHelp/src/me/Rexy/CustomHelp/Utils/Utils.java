d09287611279c6bca903dc15bc4e2aca96d591c1
Rexy <37489406+RexyTheDev@users.noreply.github.com>
2019-12-29 17:34:27 -0500

    Add files via upload

---
+++ CustomHelp/src/me/Rexy/CustomHelp/Utils/Utils.java
@@ -0,0 +1,12 @@
+package me.Rexy.CustomHelp.Utils;
+
+import org.bukkit.ChatColor;
+
+public class Utils {
+	
+	public static String chat(String s) {
+		return ChatColor.translateAlternateColorCodes('&', s);
+		
+	}
+
+}
